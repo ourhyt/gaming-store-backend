d2a7e3e55f68b962310b3e81cd15923690d906c5
Pablo <juanpablotrujillo95@gmail.com>
2024-04-17 21:53:37 -0500

    strapi app for backend

--- config/database.js
+++ config/database.js
@@ -1,11 +1,1 @@
-const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
